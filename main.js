77b55469b653383698211e463e33670f4c2885d9
anushkavhatkar <vhata986@newschool.edu>
2023-10-12 04:25:09 -0400

    added images array

---
+++ main.js
@@ -0,0 +1,148 @@
+let images = [
+    {
+      "number": 1,
+      "age": "10 DAYS OLD",
+      "item": "Onitsuka Tiger Sneakers",
+      "price": "$120"
+    },
+    {
+      "number": 2,
+      "age": "300 DAYS OLD",
+      "item": "Good Luck Hat",
+      "price": "$36"
+    },
+    {
+      "number": 3,
+      "age": "1 DAY OLD",
+      "item": "Wideboy Alarm Clock",
+      "price": "$59"
+    },
+    {
+      "number": 4,
+      "age": "2 DAYS OLD",
+      "item": "Kids Insulated Lunch Box",
+      "price": "$40"
+    },
+    {
+      "number": 5,
+      "age": "45 DAYS OLD",
+      "item": "Sweet or Sour Tee",
+      "price": "$30"
+    },
+    {
+      "number": 6,
+      "age": "60 DAYS OLD",
+      "item": "The Beauty Bag",
+      "price": "$40"
+    },
+    {
+      "number": 7,
+      "age": "5 DAYS OLD",
+      "item": "The Heart Tee",
+      "price": "$68"
+    },
+    {
+      "number": 8,
+      "age": "7 DAYS OLD",
+      "item": "Mid Century Flip Clock",
+      "price": "$95"
+    },
+    {
+      "number": 9,
+      "age": "20 DAYS OLD",
+      "item": "Rizzoli Book",
+      "price": "$30"
+    },
+    {
+      "number": 10,
+      "age": "90 DAYS OLD",
+      "item": "Gazelle Indoor Shoes",
+      "price": "$150"
+    },
+    {
+      "number": 11,
+      "age": "100 DAYS OLD",
+      "item": "New York Sweater",
+      "price": "$148"
+    },
+    {
+      "number": 12,
+      "age": "27 DAYS OLD",
+      "item": "Studio 54 Baby Tee",
+      "price": "$30"
+    },
+    {
+      "number": 13,
+      "age": "45 DAYS OLD",
+      "item": "Stussy Sweater",
+      "price": "$78"
+    },
+    {
+      "number": 14,
+      "age": "4 DAYS OLD",
+      "item": "Navy Star Sweater",
+      "price": "$40"
+    },
+    {
+      "number": 15,
+      "age": "2 DAYS OLD",
+      "item": "Icon Pattern Skirt",
+      "price": "$15"
+    },
+    {
+      "number": 16,
+      "age": "365 DAYS OLD",
+      "item": "Define Jacket",
+      "price": "$100"
+    },
+    {
+      "number": 17,
+      "age": "33 DAYS OLD",
+      "item": "Lucina Mini Dress",
+      "price": "$2,100"
+    },
+    {
+      "number": 18,
+      "age": "44 DAYS OLD",
+      "item": "Chanel Flap Bag",
+      "price": "$7,000"
+    },
+    {
+      "number": 19,
+      "age": "11 DAYS OLD",
+      "item": "Green Botsy Baguette Bag",
+      "price": "$860"
+    },
+    {
+      "number": 20,
+      "age": "13 DAYS OLD",
+      "item": "UO Paillette Bag",
+      "price": "$20"
+    },
+    {
+      "number": 21,
+      "age": "78 DAYS OLD",
+      "item": "Cozy Holiday PJs",
+      "price": "$67"
+    },
+    {
+      "number": 22,
+      "age": "100 DAYS OLD",
+      "item": "Cleo Cami",
+      "price": "$120"
+    },
+    {
+      "number": 23,
+      "age": "200 DAYS OLD",
+      "item": "Dainty Star Earrings",
+      "price": "$17"
+    },
+    {
+      "number": 24,
+      "age": "50 DAYS OLD",
+      "item": "Print Crop Top",
+      "price": "$40"
+    }
+  ];
+
+  
